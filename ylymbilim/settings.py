95ded47b47ccd2effcea0f194acb9648e56c6533
nm <nm@email.com>
2023-04-29 10:09:43 +0500

    cors change

--- ylymbilim/settings.py
+++ ylymbilim/settings.py
@@ -31,7 +31,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+CORS_ALLOWED_ORIGINS = [
+    'https://ylymbilim.com', 'https://localhost:3000'
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -48,9 +50,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -144,9 +146,5 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
-
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-]
 
 SECRET_ADMIN_URL = env('SECRET_ADMIN_URL')
